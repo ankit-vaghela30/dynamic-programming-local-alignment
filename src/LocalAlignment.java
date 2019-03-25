eec14b70b7c60072777c7c89bf7c340af6f6f808
ankit-vaghela30 <ankit.vaghela30@gmail.com>
2019-03-25 18:54:49 -0400

    final version

--- src/LocalAlignment.java
+++ src/LocalAlignment.java
@@ -1,8 +1,12 @@
 import java.io.File;
 import java.util.*;
-
+/**
+ * @author: Ankit Vaghela
+ * Search a large text for a query using Local alignment
+ * solved by using Dynamic programming approach
+ */
 public class LocalAlignment{
-
+    public static final boolean DEBUG = false;
     // query
     public String query;
 
@@ -73,6 +77,7 @@ public class LocalAlignment{
         }
     }
 
+    // Utility function to print dynamic programming table
     public void printDPTable(){
         for(int i = 0; i < dpTable.length; i++){
             for (int j = 0; j < dpTable[i].length; j++){
@@ -86,6 +91,7 @@ public class LocalAlignment{
         }
     }
 
+    // Utility function to print traceback map
     public void printTraceBackMap(){
         for(String pos : this.traceBackMap.keySet()){
             System.out.print(pos+" : ");
@@ -137,33 +143,54 @@ public class LocalAlignment{
 
     // Align two texts
     public String alignTwoText(String query, String largeText, int queryStart, int queryEnd, int largeTextStart, int largeTextEnd){
-        String beginQuery = "[" + String.valueOf(queryStart) + "]";
-        String endQuery = "[" + String.valueOf(queryEnd) + "]";
-        if(largeText.length() > query.length()){
+        
+        // prfix length used to determine which query of large text to keep fixed 
+        int prefixLength = 0;
+        
+        // Create the segment of the large text 
+        //go backwards on text
+        for(int i = largeTextStart-2; i>=0; i--){
+            if(this.largeTextString.charAt(i) == ' '){
+                largeTextStart = largeTextStart-1-i;
+                break;
+            }
+            largeText = this.largeTextString.charAt(i) + largeText;
+            prefixLength++;
+        }
+        
+        // go forward on text
+        for(int i = largeTextEnd; i<this.largeTextString.length(); i++){
+            if(this.largeTextString.charAt(i) == ' '){
+                break;
+            }
+            largeText = largeText + this.largeTextString.charAt(i);
+        }
+        
+        // keep large text as is align query accordingly
+        if(prefixLength > String.valueOf(queryStart).length()+2){
             for(int i = 0; i < largeText.length(); i++){
-                if(i < largeTextStart-1){
+                if(i < (largeTextStart - 1 - String.valueOf(queryStart).length() - 2)){
                     query = " " + query;
-                } else if(i > largeTextEnd-1){
-                    query = query + " ";
                 }
             }
         }
-        /* add the start index
-        for(int i = beginQuery.length()-1; i >= 0; i--){
-            query = beginQuery.charAt(i) + query;
-            largeText = " " + largeText;
+        
+        // keep the query as is and align large text accordingly
+        else{
+            for(int i = 0; i < query.length(); i++){
+                if(i < (queryStart - 1 + String.valueOf(queryStart).length() + 2 - (largeTextStart-1))){
+                    largeText = " " + largeText;
+                }
+            }
         }
-
-        // add the end text
-        for(int i = 0; i < endQuery.length(); i++){
-            query = query + endQuery.charAt(i);
-            largeText = largeText + " ";
-        }*/
         return (query + "\n" +largeText);
     }
 
+    // Function that prints path and final result
     public void extractMaxScoreAndPrintPath(){
         int maxNum = 0;
+
+        // Find max number
         Integer[] maxNumLoc = new Integer[2];
         for(int i = 0; i < dpTableCopy.length; i++){
             for (int j = 0; j < dpTableCopy[i].length; j++){
@@ -173,17 +200,36 @@ public class LocalAlignment{
                 }
             }
         }
+        
+        // Store initial max number location so that we can make it 0 in DP table
         Integer[] initMaxNumLoc = new Integer[]{maxNumLoc[0], maxNumLoc[1]};
-        //System.out.println(this.dpTable[5][5]);
-        if(maxNum != 0){    
-            String queryStr = ""+this.query.charAt(maxNumLoc[0]-1);
-            String bigStr = ""+this.largeTextString.charAt(maxNumLoc[1]-1);
-            String endIndexForQuery = String.valueOf(maxNumLoc[0]);
-            String endIndexForText = String.valueOf(maxNumLoc[1]);
+        if(DEBUG)System.out.println("Score is: "+maxNum);
+
+        if(maxNum != 0){
+            String queryStr = "";
+            String bigStr = "";
+            String endIndexForQuery = "";
+            String endIndexForText = "";
+            
+            // For first character 
+            if(this.query.charAt(maxNumLoc[0]-1) == this.largeTextString.charAt(maxNumLoc[1]-1)){
+                queryStr = ""+this.query.charAt(maxNumLoc[0]-1);
+                bigStr = ""+this.largeTextString.charAt(maxNumLoc[1]-1);
+            }else if(this.dpTable[maxNumLoc[0]][maxNumLoc[1]-1] != 0){
+                queryStr = queryStr+"-";
+                bigStr = bigStr + this.largeTextString.charAt(maxNumLoc[1]-1);
+            } else if(this.dpTable[maxNumLoc[0]-1][maxNumLoc[1]] != 0){
+                queryStr = queryStr + this.query.charAt(maxNumLoc[0]-1);
+                bigStr = bigStr+"-";
+            }
+            endIndexForQuery = String.valueOf(maxNumLoc[0]);
+            endIndexForText = String.valueOf(maxNumLoc[1]);
+
+            // Do recursively for rest query characters
             while(this.traceBackMap.get(maxNumLoc[0]+ "," +maxNumLoc[1]) != null && this.dpTable[this.traceBackMap.get(maxNumLoc[0]+ "," +maxNumLoc[1])[0]][this.traceBackMap.get(maxNumLoc[0]+ "," +maxNumLoc[1])[1]] != 0){
-                Integer[] prevNumLoc = new Integer[]{maxNumLoc[0], maxNumLoc[1]};
                 maxNumLoc = this.traceBackMap.get(maxNumLoc[0]+ "," +maxNumLoc[1]);
                 if(this.query.charAt(maxNumLoc[0]-1) == this.largeTextString.charAt(maxNumLoc[1]-1)){
+                    //System.out.println("reward");
                     queryStr = queryStr + this.query.charAt(maxNumLoc[0]-1);
                     bigStr = bigStr + this.largeTextString.charAt(maxNumLoc[1]-1);
                 } else if(this.dpTable[maxNumLoc[0]][maxNumLoc[1]-1] != 0){
@@ -201,32 +247,14 @@ public class LocalAlignment{
             String startIndexForquery = String.valueOf(maxNumLoc[0]);
             String startIndexForText = String.valueOf(maxNumLoc[1]);
             String displayStr = "";
+            
+            // reverse the strings
             queryStr = reverseString(queryStr);
             queryStr = "[" + startIndexForquery + "]" + queryStr + "[" + endIndexForQuery + "]";
             bigStr = reverseString(bigStr);
-            //go backwards on text
-            for(int i = Integer.valueOf(startIndexForText)-2; i>=0; i--){
-                if(this.largeTextString.charAt(i) == ' '){
-                    break;
-                }
-                bigStr = this.largeTextString.charAt(i) + bigStr;
-            }
-            // go forward on text
-            for(int i = Integer.valueOf(endIndexForText); i<this.largeTextString.length(); i++){
-                if(this.largeTextString.charAt(i) == ' '){
-                    break;
-                }
-                bigStr = bigStr + this.largeTextString.charAt(i);
-            }
-            // align
-            for(int i =0; i < ("[" + startIndexForquery + "]").length(); i++){
-                bigStr = " "+bigStr;
-            }
-            //System.out.println(queryStr);
-            //System.out.println(bigStr);
+            
+            // prints final output
             System.out.println(this.alignTwoText(queryStr, bigStr, Integer.valueOf(startIndexForquery), Integer.valueOf(endIndexForQuery), Integer.valueOf(startIndexForText), Integer.valueOf(endIndexForText)));
-            //System.out.println(queryStr);
-            //System.out.println(bigStr);
             this.dpTableCopy[initMaxNumLoc[0]][initMaxNumLoc[1]] = 0;
         }else{
             System.out.println("Everything is 0!");
@@ -244,20 +272,29 @@ public class LocalAlignment{
     public static void main(String[] args) {
         LocalAlignment localAlignment = new LocalAlignment(args);
         try{
-            System.out.println(localAlignment.query);
-            System.out.println(localAlignment.k);
-            System.out.println(localAlignment.largeTextString);
-
+            if(DEBUG){
+                System.out.println(localAlignment.query);
+                System.out.println(localAlignment.k);
+                System.out.println(localAlignment.largeTextString);
+            }
+            
             // Create Dynamic programming table
             localAlignment.createDynamicProgrammingTable();
-            localAlignment.printDPTable();
-            localAlignment.printTraceBackMap();
-
-            // extract paths according to k
-            for(int i = 0; i < localAlignment.k; i++){
-                System.out.println("hit : "+i);
-                localAlignment.extractMaxScoreAndPrintPath();
+            if(DEBUG){
+                localAlignment.printDPTable();
+                localAlignment.printTraceBackMap();
             }
+
+            // extract k maximum paths
+            //System.out.println("--------------------------------------------------------");
+            for(int i = 0; i < localAlignment.k; i++){
+                System.out.println("--------------------------------------------------------");
+                System.out.println("hit : "+(i+1));
+                System.out.println();
+                localAlignment.extractMaxScoreAndPrintPath();
+                System.out.println("--------------------------------------------------------");
+            }
+            //System.out.println("--------------------------------------------------------");
         } catch(Exception e){
             e.printStackTrace();
         }
